7d67f492f9b91bb1b9ddeb62649b14f52aae8e4f
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-13 20:32:50 -0600

    add pair submission

---
+++ challenge/group.rb
@@ -0,0 +1,12 @@
+#we titled this 'group' because it is a group challenge
+title = 'the_wild_girl'
+
+author = 'kate_forsyth'
+
+hours = 18.0
+#declare availability status
+available = true
+#declare publishing company
+publisher = 'blackstone_publishing'
+format = 'audiobook'
+genre = ['fiction', 'literature', 'historical_fiction']
---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+# Book Titles
+
+1. The Bible
+2. The Filter Bubble
+3. Code Name
+4. The Red Ribbon
+5. The Wild Girl
+
+# Workflow
+- Driver: Kaelin
+- Navigators: Kristen, Aleisha
